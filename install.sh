54b732fa0117aad7612d6000875b1cf59f77762c
James Anderson <james.anderson2@btinternet.com>
2022-06-02 12:58:07 +0100

    Created install bash script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Script to install Linux System Tools
+# and basic Python components
+
+# General linux
+apt-get update
+apt-get upgrade -y
+apt-get install -y bzip2 gcc git htop wget screen nano
+apt-get upgrade -y bash
+apt-get clean
+
+# Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O \
+    Miniconda.sh
+bash Miniconda.sh -bash
+rm -rf Miniconda.sh
+export PATH="/root/miniconda3/bin:$PATH"
+
+# Python libraries
+conda install -y pandas
+conda install -y ipython
